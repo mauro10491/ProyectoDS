3ae376e9e40c89d287c3df20d2f09df8c7bd390a
Mauricio <mauricio.alzate13701@udecaldas.edu.co>
2023-05-06 14:36:53 -0500

    Nuevos cambios

--- ProyectoDS/src/ventanas/Administrador.java
+++ ProyectoDS/src/ventanas/Administrador.java
@@ -4,20 +4,62 @@
  */
 package ventanas;
 
+import java.sql.*;
+import clases.Conexion; 
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.WindowConstants;
 /**
  *
  * @author mauri
  */
 public class Administrador extends javax.swing.JFrame {
+    
+    String user, nombre_usuario;
+    public static int sesion_usuario;
 
     /**
      * Creates new form Administrador
      */
     public Administrador() {
         initComponents();
-        setTitle("Administrador");
+        user = Login.user;
+        sesion_usuario = 1;
+        
+        setSize(650,430);
+        setResizable(false);
+        
+        setTitle("Administrador - sesion de " + user);
         setLocationRelativeTo(null);
         
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        ImageIcon wallpaper = new ImageIcon("src/images/wallpaperPrincipal.jpg");
+        Icon icono = new ImageIcon(wallpaper.getImage().getScaledInstance(jLabel_wallpaper.getWidth(), jLabel_wallpaper.getHeight(), Image.SCALE_DEFAULT));
+        jLabel_wallpaper.setIcon(icono);
+        this.repaint();
+        
+        try {
+            Connection cn = Conexion.conectar();
+            PreparedStatement pst = cn.prepareStatement("select nombre_usuario from usuarios where username = '" + user + "' ");
+            
+            ResultSet rs = pst.executeQuery();
+            
+            if (rs.next()) {
+                nombre_usuario = rs.getString("nombre_usuario");
+                jLabel_nombreusuario.setText(nombre_usuario);
+            }
+        } catch (SQLException e) {
+            System.err.println("Error en conexion desde la interfaz administrador");
+        } 
+    }
+    
+    @Override
+    public Image getIconImage(){
+        Image retValue = Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("images/icon.png"));
+        return retValue;
     }
 
     /**
@@ -29,21 +71,99 @@ public class Administrador extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jLabel_nombreusuario = new javax.swing.JLabel();
+        jButton_registrarusuario = new javax.swing.JButton();
+        jButton_Gestionarusuarios = new javax.swing.JButton();
+        jButton_Creatividad = new javax.swing.JButton();
+        jButton_capturista = new javax.swing.JButton();
+        jButton_tecnico = new javax.swing.JButton();
+        jButton_Acercede = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel_wallpaper = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setIconImage(getIconImage());
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel_nombreusuario.setFont(new java.awt.Font("Arial", 1, 20)); // NOI18N
+        jLabel_nombreusuario.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel_nombreusuario.setText("jLabel1");
+        getContentPane().add(jLabel_nombreusuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, -1, -1));
+
+        jButton_registrarusuario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/addUser.png"))); // NOI18N
+        jButton_registrarusuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_registrarusuarioActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton_registrarusuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 70, 120, 100));
+
+        jButton_Gestionarusuarios.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/informationuser.png"))); // NOI18N
+        getContentPane().add(jButton_Gestionarusuarios, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 70, 120, 100));
+
+        jButton_Creatividad.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/creatividad.png"))); // NOI18N
+        jButton_Creatividad.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_CreatividadActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton_Creatividad, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 70, 120, 100));
+
+        jButton_capturista.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/capturista.png"))); // NOI18N
+        getContentPane().add(jButton_capturista, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 240, 120, 100));
+
+        jButton_tecnico.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/tecnico.png"))); // NOI18N
+        getContentPane().add(jButton_tecnico, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 240, 120, 100));
+
+        jButton_Acercede.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/geekipedia.png"))); // NOI18N
+        getContentPane().add(jButton_Acercede, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 240, 120, 100));
+
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Registrar Usuario");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 170, -1, -1));
+
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Gestionar Usuarios");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 170, -1, -1));
+
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Creatividad");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 170, -1, -1));
+
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Panel Vista Capturista");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 340, -1, -1));
+
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("Panel Vista Tecnico");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 340, -1, -1));
+
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Acerca De");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 340, -1, -1));
+
+        jLabel7.setText("Creado por Mauricio");
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 370, -1, -1));
+        getContentPane().add(jLabel_wallpaper, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 650, 430));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton_CreatividadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_CreatividadActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton_CreatividadActionPerformed
+
+    private void jButton_registrarusuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_registrarusuarioActionPerformed
+        RegistrarUsuarios registrarUsuarios = new RegistrarUsuarios();
+        registrarUsuarios.setVisible(true);
+        
+    }//GEN-LAST:event_jButton_registrarusuarioActionPerformed
 
     /**
      * @param args the command line arguments
@@ -81,5 +201,20 @@ public class Administrador extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton_Acercede;
+    private javax.swing.JButton jButton_Creatividad;
+    private javax.swing.JButton jButton_Gestionarusuarios;
+    private javax.swing.JButton jButton_capturista;
+    private javax.swing.JButton jButton_registrarusuario;
+    private javax.swing.JButton jButton_tecnico;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel_nombreusuario;
+    private javax.swing.JLabel jLabel_wallpaper;
     // End of variables declaration//GEN-END:variables
 }
